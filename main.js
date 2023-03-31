303732695940ba99f1352f2721013de6a2f2786a
Helder Paiva <helder.devjava@gmail.com>
2023-03-30 22:51:19 -0300

    função para testar os pares

--- main.js
+++ main.js
@@ -1,5 +1,50 @@
-const opcoes = ["Rock", "Paper", "Scissors"];
+const opcoes = ["rock", "paper", "scissors"];
 
 function getComputerChoice(){
-  console.log(opcoes[Math.floor(Math.random() * 3)]);
+  return opcoes[Math.floor(Math.random() * 3)];
+}
+
+function play(playerSelection, computerSelection) {
+  let player = playerSelection.toLowerCase();
+  let comput = computerSelection.toLowerCase();
+
+  switch(player) {
+    case "rock":
+      if(computerSelection == "rock"){
+        return 'empate';
+      } else if(computerSelection == "scissors"){
+        return 'vencedor'
+      } else {
+        return 'perdedor'
+      }
+
+    case "paper":
+      if(computerSelection == "rock"){
+        return 'vencedor';
+      } else if(computerSelection == "scissors"){
+        return 'perdedor'
+      } else {
+        return 'empate'
+      }
+
+    case "scissors":
+      if(computerSelection == "rock"){
+        return 'perdedor';
+      } else if(computerSelection == "scissors"){
+        return 'empate'
+      } else {
+        return 'vencedor'
+      }
+
+    default:
+        return "palavra errada";
+  }
+}
+
+function resWin(){
+  return 
+}
+
+function resLose(){
+  return 
 }
