2bba6865f7d2a0c568048f061b51d48aad8bc169
Helder Paiva <helder.devjava@gmail.com>
2023-04-10 12:12:34 -0300

    finalizando o jogo, adicionado muitos metodos

--- main.js
+++ main.js
@@ -1,14 +1,58 @@
 const opcoes = ["rock", "paper", "scissors"];
 
+const outputPlayer = document.querySelector('.pPlayer');
+outputPlayer.textContent = 0;
+
+const outputComput = document.querySelector('.pComput');
+outputComput.textContent = 0;
+
+const outputPartidas = document.querySelector('.pPartidas');
+outputPartidas.textContent = 0;
+
+const h1 = document.querySelector('h1');
+
+let player = 0, comput = 0, plays = 0;
+
 function getComputerChoice(){
   return opcoes[Math.floor(Math.random() * 3)];
 }
 
-function play(playerSelection, computerSelection) {
-  let player = playerSelection.toLowerCase();
-  let comput = computerSelection.toLowerCase();
+const buttons = document.querySelectorAll("button");
 
-  switch(player) {
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    let person = button.className;
+    let pc = getComputerChoice();
+    console.log(person, pc);
+    if(plays <= 5) {
+      result = play(person, pc)
+      if(result == "vencedor"){
+        addUmPlayer();
+        game();
+      } else if (result == "perdedor"){
+        AddUmComput();
+        game();
+      }
+      
+       if (plays == 5 || Math.abs(player - comput) == 3 || (plays == 4 && Math.abs(player - comput) == 2)){
+        if(player > comput){
+          win();
+        } else {
+          lose()
+        }
+       }
+    }
+    console.log('jogos:'+plays, 'pontos player:'+player, 'pontos computador:'+comput);
+  });
+});
+
+
+function play(playerSelection, computerSelection) {
+  // let player = playerSelection;
+  // player = player.toLowerCase();
+  // let comput = computerSelection.toLowerCase();
+
+  switch(playerSelection) {
     case "rock":
       if(computerSelection == "rock"){
         return 'empate';
@@ -41,10 +85,37 @@ function play(playerSelection, computerSelection) {
   }
 }
 
-function resWin(){
-  return 
+function game() {
+  plays+=1;
+  outputPartidas.textContent = plays;
 }
 
-function resLose(){
-  return 
+function win(){
+  h1.textContent = `VOCÊ GANHOU DE ${player} X ${comput}`
+  buttons.forEach(button => {
+    button.disabled = true;
+  });
+  //player = 0, comput = 0, plays = 0;
+  outputPlayer.textContent = player
+  outputComput.textContent = comput;
+}
+
+function lose(){
+  h1.textContent = `VOCÊ PERDEU DE ${player} X ${comput}`
+  buttons.forEach(button => {
+    button.disabled = true;
+  });
+  //player = 0, comput = 0, plays = 0;
+  outputPlayer.textContent = player
+  outputComput.textContent = comput;
+
+}
+function addUmPlayer(){
+  player +=1; 
+  outputPlayer.textContent = player
+}
+
+function AddUmComput(){
+  comput+=1; 
+  outputComput.textContent = comput;
 }
